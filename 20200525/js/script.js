f2f9c393a5e3b59ff90f42f503fe6ec78e9970be
baesukyung <64953394+baesukyung@users.noreply.github.com>
2020-05-27 15:15:00 +0900

    조별

--- 20200525/js/script.js
+++ 20200525/js/script.js
@@ -20,6 +20,7 @@
 // });
 
 $(function(){
+    //메뉴up down
     $(".menu").mouseover(function(){
         $(".subNav").stop().slideDown();
         $(".subNav").css("display","flex");
@@ -40,4 +41,30 @@ setInterval(function(){
     $(".slide_box").delay("2500");
     $(".slide_box").animate({marginTop:"0"},"500");
 });
+//슬라이드
 
+
+
+$(function(){
+    $("#tab1").click(function(){
+        $("#tab1").css("background","lightcoral");
+        $("#tab2").css("background","none");
+        $(".tab-1").css("display","block");
+        $(".tab-2").css("display","none");
+    });
+    $("#tab2").click(function(){
+        $("#tab2").css("background","lightcoral");
+        $("#tab1").css("background","none");
+        $(".tab-2").css("display","block");
+        $(".tab-1").css("display","none");
+    });
+});
+
+$(function(){
+    $(".popup_button").on('click',function(){
+        $('.popup').fadeIn();
+    });
+    $(".팝업닫기").on('click',function(){
+        $('.popup').fadeOut();
+    });
+});

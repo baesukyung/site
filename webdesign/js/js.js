e99edc1e5ef5a3b54b8fcc8b505c9a992dd7556c
baesukyung <64953394+baesukyung@users.noreply.github.com>
2020-06-02 17:56:41 +0900

    ㄴㅇㄹ
    ㄴㅇㄹ

--- webdesign/js/js.js
+++ webdesign/js/js.js
@@ -1,7 +1,5 @@
-// // 슬라이드
-// setInterval(function(){
-//     $(".slide-body")
-// });
+
+
 
 // 메뉴슬라이드
 $(function(){
@@ -14,4 +12,35 @@ $(function(){
     });
 });
 
+// // 사진 슬라이드
+setInterval(function(){
+    $(".slide-img").delay("2500");
+    $(".slide-img").animate({marginTop:"-300px"},500);
+    $(".slide-img").delay("2500");
+    $(".slide-img").animate({marginTop:"-600px"},500);
+    $(".slide-img").delay("2500");
+    $(".slide-img").animate({marginTop:"0"},500);
 
+
+});
+
+
+
+
+
+
+
+$(function(){
+    $("#notice1").click(function(){
+        $("#notice1").css("background","lightcoral");
+        $("#gallery1").css("background","none");
+        $(".notice").css("display","block");
+        $(".gallery").css("display","none");
+    });
+    $("#gallery1").click(function(){
+        $("#gallery1").css("background","lightcoral");
+        $("#notice1").css("background","none");
+        $(".gallery").css("display","block");
+        $(".notice").css("display","none");
+    });
+});

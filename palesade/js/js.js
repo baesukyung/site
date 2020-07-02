439baf0e013e807142c895c6b33caed49d48195a
Bellin61 <hjtoi519@gmail.com>
2020-07-02 09:19:43 +0900

    palisade

--- palesade/js/js.js
+++ palesade/js/js.js
@@ -104,6 +104,63 @@ $('.modal-img1').mouseleave(function(){
     $('.whitecar-txt').hide();
 });
 AOS.init();
-new Swiper('.swiper-container');
+
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+     
+    },
+  });
+  $(document).ready(function(){
+    $(".bg01").click(function(){
+      $("#myModal01").modal();
+    });
+    $(".bg02").click(function(){
+        $("#myModal02").modal();
+      });
+      $(".bg03").click(function(){
+        $("#myModal03").modal();
+      });
+      $(".bg04").click(function(){
+        $("#myModal04").modal();
+      });
+      $(".bg05").click(function(){
+        $("#myModal05").modal();
+      });
+      $(".bg06").click(function(){
+        $("#myModal06").modal();
+      });
+  });
+
+$('#menu1').click(function(){
+$('#ex01').css('display','block');
+$('#ex02').css('display','none');
+$('#ex03').css('display','none');
+$('#ex04').css('display','none');
+});
+$('#menu2').click(function(){
+    $('#ex02').css('display','block');
+    $('#ex01').css('display','none');
+    $('#ex03').css('display','none');
+    $('#ex04').css('display','none');
+    });
+    $('#menu3').click(function(){
+     $('#ex03').css('display','block');
+    $('#ex02').css('display','none');
+    $('#ex01').css('display','none');
+    $('#ex04').css('display','none');
+    });
+    $('#menu4').click(function(){
+    $('#ex04').css('display','block');
+    $('#ex02').css('display','none');
+    $('#ex03').css('display','none');
+    $('#ex01').css('display','none');
+    });       
 });
 

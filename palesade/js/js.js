d087774366f5917f5aafc3d578cafe57919f0c3d
Bellin61 <hjtoi519@gmail.com>
2020-07-03 14:18:35 +0900

    palisade

--- palesade/js/js.js
+++ palesade/js/js.js
@@ -171,4 +171,12 @@ $('.conven02').click(function(){
     $('#ex01').css('display','none');
     });       
 });
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+      verticalCentered: true,
+      anchors: ['anchor1', 'anchor2', 'anchor3'],
+      menu: '#menu',
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE']
+  });
+});
 

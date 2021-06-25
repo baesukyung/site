e7035594fca20ee3774c3e65bc0234b18ac8a3f9
baesukyung <owlcity06@naver.com>
2021-06-26 04:25:57 +0900

    s

---
+++ baesukyung/sub/js/js.js
@@ -0,0 +1,45 @@
+
+$(function(){
+	'use strict';
+
+/*--------------------------------------------------
+    Initialize portfolio filter
+    ---------------------------------------------------*/
+	$('#portfolio-grid').mixItUp();
+
+/*--------------------------------------------------
+    Open video modal
+    ---------------------------------------------------*/
+	$('#popup-youtube').magnificPopup({
+	    disableOn: 700,
+	    type: 'iframe',
+	    mainClass: 'mfp-fade',
+	    removalDelay: 160,
+	    preloader: false,
+	    fixedContentPos: false
+	});
+	
+/*--------------------------------------------------
+    Page Scroll Features 
+    ---------------------------------------------------*/
+	smoothScroll.init({
+        speed: 1000,
+        updateURL: true,
+        offset: 50
+    });
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 130) {
+            $('#header-nav').addClass('navbar-wooster');
+        } else {
+            $('#header-nav').removeClass('navbar-wooster');
+        }
+    });
+
+});
+$(function(){
+    $('.mobile-btn').click(function(){
+        $(this).toggleClass('active');
+        $(".menudrop").toggle();
+    });
+});
